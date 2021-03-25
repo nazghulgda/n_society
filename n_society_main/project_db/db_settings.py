5c257cbcb17d54509fa9332185a021b89e22a71c
Jarek Klamann <jaroslaw.klamann@serv.net.pl>
2021-03-25 17:21:41 +0100

    project started, db settings added

---
+++ n_society_main/project_db/db_settings.py
@@ -0,0 +1,7 @@
+dbinfo = {
+        'HOST': '127.0.0.1',
+        'NAME': 'db_n_society',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'n_society',
+        'PASSWORD': 'n_society_01',
+}
